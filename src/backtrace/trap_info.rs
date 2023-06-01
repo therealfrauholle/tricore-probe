74ea030158065d1110c5cfec03d01521eb1554a3
Finn Hermeling <finn.hermeling@elexir.eu>
2023-06-01 21:11:47 +0200

    Improve documentation

--- src/backtrace/trap_info.rs
+++ src/backtrace/trap_info.rs
@@ -3,14 +3,19 @@ use std::path::Path;
 use anyhow::Context;
 use elf::{endian::AnyEndian, ElfBytes};
 
+/// Information about where the trap table is located
 pub struct TrapMetadata {
+    /// The start of the trap table as an absolute address in code sapce
     trap_symbol: u32,
 }
 
 impl TrapMetadata {
-    /// Check for the first_trap_table symbol to infer trap information from PC
+    /// Infer information about the trap table by looking into the elf file
+    /// 
+    /// FIXME: This is a workaround that relies on implementation details to work,
+    /// the proper way to provide this functionality is to read the BTV register on
+    /// the device.
     pub fn from_elf(elf_file: &Path) -> anyhow::Result<Self> {
-        // first_trap_table
         let elf_data = std::fs::read(elf_file).unwrap();
         let elf = ElfBytes::<'_, AnyEndian>::minimal_parse(&elf_data).unwrap();
 
@@ -39,14 +44,18 @@ impl TrapMetadata {
         })
     }
 
-    pub fn trap_class(&self, address: u32) -> Option<u8> {
-        let offset_from_start = address.checked_sub(self.trap_symbol)?;
+    /// Attempt to infer the trap class based on the given program counter
+    /// 
+    /// The program counter must still point into the trap service routine defined int
+    /// the trap table itself.
+    pub fn trap_class(&self, program_counter: u32) -> Option<u8> {
+        let offset_in_trap_table = program_counter.checked_sub(self.trap_symbol)?;
 
-        if offset_from_start > 8 * 32 {
+        if offset_in_trap_table > 8 * 32 {
             return None;
         }
 
-        let class = offset_from_start / 32;
+        let class = offset_in_trap_table / 32;
 
         Some(class as u8)
     }
